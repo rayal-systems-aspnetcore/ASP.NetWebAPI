8fc292124631f4f6f8a9cb88eeb92c2348703a4e
= <=>
2020-06-19 12:41:20 +0530

    ASP.NET Web API with jQuery Client - 2

---
+++ WebAPIDemoApp/WebAPIDemoApp/Controllers/EmpClientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPIDemoApp.Controllers
+{
+    public class EmpClientController : Controller
+    {
+        // GET: EmpClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
