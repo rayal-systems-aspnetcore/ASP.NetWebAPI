495f3e644a844addc687bba8118dbed102cba220
= <=>
2020-06-19 10:03:15 +0530

    ASP.NET Web API with jQuery Client

--- WebAPIDemoApp/WebAPIDemoApp/Controllers/ValuesController.cs
+++ WebAPIDemoApp/WebAPIDemoApp/Controllers/ValuesController.cs
@@ -7,33 +7,53 @@ using System.Web.Http;
 
 namespace WebAPIDemoApp.Controllers
 {
+    [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
+
+        public static List<string> values = new List<string>()
+        {
+            "Advanced Video Upscaling, Compression and Enhancement",
+            "Performance analysis and monitoring tools for Android",
+            "Special-effects titling software for film and video",
+            "In-app mobile messaging and push notification tools",
+            "Bioinformatics solutions for life science research",
+            "Application discovery and dashboard visualization",
+            "Mobile business intelligence & Data visualization" 
+        };
         // GET api/values
+        [Route("")]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return values;
         }
 
+        [Route("{ind}")]
         // GET api/values/5
-        public string Get(int id)
+        public string Get(int ind)
         {
-            return "value";
+            return values[ind];
         }
 
+        [Route("")]
         // POST api/values
         public void Post([FromBody] string value)
         {
+            values.Add(value);
         }
 
+        [Route("{ind}")]
         // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
+        public void Put(int ind, [FromBody] string value)
         {
+            values[ind] = value;
         }
 
+        [Route("{ind}")]
         // DELETE api/values/5
-        public void Delete(int id)
+        public void Delete(int ind)
         {
-        }
+            values.RemoveAt(ind);
+        } 
     }
 }
